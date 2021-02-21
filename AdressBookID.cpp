9a154e3dd9890ed36a988a6bf81cc59b83a879aa
AnastasiaGolomazova <66026569+AnastasiaGolomazova@users.noreply.github.com>
2021-02-21 12:04:33 +0300

    init commit

---
+++ AdressBookID.cpp
@@ -0,0 +1,6 @@
+#include "AdressBookID.h"
+
+Book::Book()
+{
+
+}
---
+++ AdressBookID.h
@@ -0,0 +1,12 @@
+#ifndef BOOK_H
+#define BOOK_H
+
+#include <QWidget>
+
+class Book
+{
+public:
+    Book();
+};
+
+#endif // BOOK_H
